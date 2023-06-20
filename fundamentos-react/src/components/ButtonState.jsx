5a4b0038999d5ff60fb9f3798879f8b26160950d
evans2919 <evansalcantara29@outlook.com>
2023-06-20 19:49:12 +0200

    Añadiendo función anónima directamente en el botón para evitar líneas de código, es más legible tener la función por fuera y llamarla con el evento clic

--- fundamentos-react/src/components/ButtonState.jsx
+++ fundamentos-react/src/components/ButtonState.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 
 const ButtonState = () => {
     const [count, setCount] = useState(0);
+
     const handleClick = () => { 
         setCount(count + 1);
     }
+
     return (
-        <button onClick={handleClick}>Counter: { count } </button>
-    );
+        <button onClick={handleClick}>Counter: { count }</button>
+    ); 
+    
+    // return (
+    //     <button onClick={() => setCount((count) => count + 1 )}>Counter: { count }</button>
+    // );
+
 
 }
 
