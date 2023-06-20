bb2681a97b8961bd8ddddc6a6c033bc0ef18a9a1
evans2919 <evansalcantara29@outlook.com>
2023-06-20 19:22:14 +0200

    Refactorizando componente ButtonState añadiendo desestructuración al hook useState - FUNCIONA

--- fundamentos-react/src/components/ButtonState.jsx
+++ fundamentos-react/src/components/ButtonState.jsx
@@ -1,22 +1,14 @@
-import PropTypes from "prop-types";
 import { useState } from "react";
 
 const ButtonState = () => {
-
-    let count = 0;
-    
-    const arrayState = useState(0);
-    console.log (arrayState);
-    const stateNumber = arrayState[0];
-    const stateFunction = arrayState[1];
-
-
-    const handleClick = () => {
-        // count = count + 1;
-        // console.log("Button count: " + count);
-        stateFunction(stateNumber + 1);
+    const [count, setCount] = useState(0);
+    const handleClick = () => { 
+        setCount(count + 1);
     }
-    return <button onClick={handleClick}>buttonstate: {stateNumber} </button>;
-};
+    return (
+        <button onClick={handleClick}>Counter: { count } </button>
+    );
+
+}
 
 export default ButtonState;
