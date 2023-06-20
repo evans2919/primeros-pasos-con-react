b87eedb93ed31b1922eb7b2d080450d23e4d6e35
Evans Alcántara <evans.alcantara@avannubo.com>
2023-06-20 17:03:26 +0200

    Añadiendo useState al componente ButtonState para el contador

--- fundamentos-react/src/components/ButtonState.jsx
+++ fundamentos-react/src/components/ButtonState.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const ButtonState = () => {
 
     let count = 0;
+    
+    const arrayState = useState(0);
+    console.log (arrayState);
+    const stateNumber = arrayState[0];
+    const stateFunction = arrayState[1];
+
 
     const handleClick = () => {
-        count = count + 1;
-        console.log("Button count: " + count);
+        // count = count + 1;
+        // console.log("Button count: " + count);
+        stateFunction(stateNumber + 1);
     }
-    return <button onClick={handleClick}>buttonstate: {count} </button>;
+    return <button onClick={handleClick}>buttonstate: {stateNumber} </button>;
 };
 
 export default ButtonState;
--- fundamentos-react/src/components/MyButton.jsx
+++ fundamentos-react/src/components/MyButton.jsx
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 const MyButton = ({ title, number }) => {
   const handleClick = (title, number) => {
     console.log("Haz hecho clic en: " + title + " " + number);
-  };
+  }
   return (
     <button onClick={() => handleClick(title, number)}>
-      {title} {number}
+      {title}{number}
     </button>
   );
-};
+} 
+
 
 MyButton.propTypes = {
   title: PropTypes.string.isRequired,
