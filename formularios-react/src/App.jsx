ca9f1f91086fc6f746253d5b331632e7cb35610a
evans2919 <evansalcantara29@outlook.com>
2023-06-20 20:49:55 +0200

    Primeros pasos de creación de formularios

--- formularios-react/src/App.jsx
+++ formularios-react/src/App.jsx
@@ -3,6 +3,7 @@ const App = () => {
   return (
 
     <div className="container">
+    <h1>Formulario no controlado</h1>
     <NotControlledForm></NotControlledForm>
     </div>
 
--- formularios-react/src/components/NotControlledForm.jsx
+++ formularios-react/src/components/NotControlledForm.jsx
@@ -1,7 +1,31 @@
 const NotControlledForm = () => {
-    return (
-        "Formulario no controlado"
-    );
-}
+  return (
+    <form action="">
+      <input
+        type="text"
+        placeholder="Ingrese ToDo"
+        className="form-control mb-2"
+        name="name"
+      />
+      <textarea
+        placeholder="Ingrese descripción"
+        className="form-control mb-2"
+        name="description"
+      />
+      <select
+        className="form-select mb-2"
+        defaultValue="completado"
+        name="state"
+      >
+        <option value="">Elija una opción</option>
+        <option value="pendiente">Pendiente</option>
+        <option value="procesado">Procesado</option>
+      </select>
+      <button type="submit" className="btn btn-primary">
+        Procesar
+      </button>
+    </form>
+  );
+};
 
 export default NotControlledForm;
