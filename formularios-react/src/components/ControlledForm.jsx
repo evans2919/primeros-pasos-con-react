971cc02e2f9fe9dec762e93ba999b1c12933acc4
evans2919 <evansalcantara29@outlook.com>
2023-06-24 13:16:56 +0200

    Refactorizando componente utilizando hook useState como objeto y pasándolo en campo value y en método onChange del formulario

--- formularios-react/src/components/ControlledForm.jsx
+++ formularios-react/src/components/ControlledForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 const ControlledForm = () => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [state, setState] = useState("pendiente");
+  const [ToDo, setToDo] = useState({
+    name: "",
+    description: "",
+    state: "pendiente",
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,21 +18,21 @@ const ControlledForm = () => {
         placeholder="Ingrese ToDo"
         className="form-control mb-2"
         name="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
+        value={ToDo.name}
+        onChange={(e) => setToDo({ ...ToDo, name: e.target.value })}
       />
       <textarea
         placeholder="Ingrese descripción"
         className="form-control mb-2"
         name="description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+        value={ToDo.description}
+        onChange={(e) => setToDo({ ...ToDo, description: e.target.value })}
       />
       <select
         className="form-select mb-2"
         name="state"
-        value={state}
-        onChange={(e) => setState(e.target.value)}
+        value={ToDo.state}
+        onChange={(e) => setToDo({ ...ToDo, state: e.target.value })}
       >
         <option>Elija una opción</option>
         <option value="pendiente">Pendiente</option>
