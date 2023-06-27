517fa9759bebb4a29e7c1a7acc2a40de6071c166
evans2919 <evansalcantara29@outlook.com>
2023-06-27 20:41:35 +0200

    Importación ToDoItem a ToDoList

--- react-to-do-app/src/components/ToDoList.jsx
+++ react-to-do-app/src/components/ToDoList.jsx
@@ -1,12 +1,12 @@
+import ToDoListItem from "./ToDoListItem";
+
 const ToDoList = ({ ToDo }) => {
   return (
     <div className="mt-5">
       <h2 className="text-center">Lista de tareas</h2>
       <ul className="list-group">
         {ToDo.map((ToDo) => (
-          <li key={ToDo.id} className="list-group-item">
-            {ToDo.name}
-          </li>
+          <ToDoListItem key={ToDo.id} ToDo={ToDo}></ToDoListItem>
         ))}
       </ul>
     </div>
