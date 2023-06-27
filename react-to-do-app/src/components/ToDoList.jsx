488f138f2f0ddb29a23acfa3d17831c4d2f22d22
evans2919 <evansalcantara29@outlook.com>
2023-06-27 21:15:45 +0200

    Creando condicional en lista para cuando se eliminen todas las tareas

--- react-to-do-app/src/components/ToDoList.jsx
+++ react-to-do-app/src/components/ToDoList.jsx
@@ -1,14 +1,22 @@
+/* eslint-disable react/prop-types */
 import ToDoListItem from "./ToDoListItem";
 
-const ToDoList = ({ ToDo }) => {
+const ToDoList = ({ ToDo, deleteToDo }) => {
   return (
     <div className="mt-5">
-      <h2 className="text-center">Lista de tareas</h2>
+      <h2 className="text-center my-5">Lista de tareas</h2>
       <ul className="list-group">
         {ToDo.map((ToDo) => (
-          <ToDoListItem key={ToDo.id} ToDo={ToDo}></ToDoListItem>
+          <ToDoListItem key={ToDo.id} ToDo={ToDo} deleteToDo={deleteToDo}></ToDoListItem>
         ))}
       </ul>
+      {ToDo.length === 0 &&
+        <ul className="list-group">
+          <li className="list-group-item text-center">
+            No existen tareas creadas
+          </li>
+      
+      </ul>}
     </div>
   );
 };
