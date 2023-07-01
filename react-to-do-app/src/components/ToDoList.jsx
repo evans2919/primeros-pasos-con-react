5729f52cb7e1ba7896641c126893408b7e4a7f7a
evans2919 <evansalcantara29@outlook.com>
2023-07-01 08:18:39 +0200

    Título y botón para eliminar todas las tareas

--- react-to-do-app/src/components/ToDoList.jsx
+++ react-to-do-app/src/components/ToDoList.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
 import ToDoListItem from "./ToDoListItem";
-const ToDoList = ({ toDo, updateToDo, deleteToDo }) => {
+const ToDoList = ({ toDo, updateToDo, deleteToDo, deleteAll }) => {
   return (
     <>
-      <ul className="list-group">
+      <h2 className="mt-5 text-center">Lista de tareas</h2>
+      {toDo.length > 0 && (
+        <div className="d-flex justify-content-end my-5">
+          <div>
+            <p>Tareas creadas: {toDo.length}</p>
+            {toDo.length > 1 && (
+              <button className="btn btn-danger" onClick={() => deleteAll()}>
+                Eliminar todas las tareas
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+      <div className="row">
         {toDo.map((toDo) => (
           <ToDoListItem
             key={toDo.id}
@@ -12,11 +25,11 @@ const ToDoList = ({ toDo, updateToDo, deleteToDo }) => {
             deleteToDo={deleteToDo}
           ></ToDoListItem>
         ))}
-      </ul>
+      </div>
       {toDo.length === 0 && (
         <ul className="list-group">
           <li className="list-group-item shadow-sm p-3 mb-5 bg-body rounded text-center">
-            No existen tareas creadas
+            Actualmente no existen tareas creadas.
           </li>
         </ul>
       )}
